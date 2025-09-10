c8b3de266526fe7c6d66dd8cd4405ef716c050cc
ALPHA YashClg <yashclguse@gmail.com>
2025-09-10 11:09:50 +0530

    Timep.py

---
+++ Tp.py
@@ -0,0 +1,5 @@
+print("To find if the input is character, number or special characters")
+x = input("Enter to check : ")
+# if x == (a <= z and A <= Z):
+if x >= 'a' and x <= 'z' or x >= 'A' and x <= 'Z':
+  print("It is a Character")
